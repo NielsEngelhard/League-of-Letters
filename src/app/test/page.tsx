dd3d7ecce776ff751c5718f5788c891afb63b527
engelhardniels <engelhardniels@gmail.com>
2025-08-04 20:52:07 +0200

    test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,46 +1,14 @@
 "use client"
 
 import PageBase from "@/components/layout/PageBase";
-import GameResultOverview from "@/features/game/components/GameResultOverview";
-import { GamePlayerModel } from "@/features/game/game-models";
 
 export default function TestPage() {
     
-    const players: GamePlayerModel[] = [
-        {
-            id: "1",
-            score: 101,
-             username: "waterpolo"
-        },
-        {
-            id: "5",
-            score: 1044,
-             username: "HeleLangeUsernameee"
-        },
-        {
-            id: "3",
-            score: 2,
-             username: "koekjesdeeg"
-        },
-        {
-            id: "2",
-            score: 22,
-             username: "asdadsad"
-        },
-        {
-            id: "1123123",
-            score: 23123,
-             username: "WarmChipmunk20032"
-        }                                
-    ]
-    
     return (
         <PageBase>
-            <GameResultOverview
-                players={players}
-            >
-
-            </GameResultOverview>
+            <div>
+                testing
+            </div>
         </PageBase>
     )
 }
