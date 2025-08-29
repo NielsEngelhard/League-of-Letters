2ca7409a0e1b70de0f52a1668aeaf058d35b9c35
engelhardniels <engelhardniels@gmail.com>
2025-08-29 14:57:44 +0200

    fix online game state

--- letter-league/src/app/[lang]/game/[gameId]/page.tsx
+++ letter-league/src/app/[lang]/game/[gameId]/page.tsx
@@ -2,16 +2,10 @@
 
 import { LANGUAGE_ROUTE, PICK_GAME_MODE_ROUTE } from "@/app/routes";
 import PageBase from "@/components/layout/PageBase";
-import LoadingSpinner from "@/components/ui/animation/LoadingSpinner";
-import { useAuth } from "@/features/auth/AuthContext";
 import { GetActiveGameByIdRequest } from "@/features/game/actions/query/get-active-game-by-id-request";
-import { useActiveGame } from "@/features/game/components/active-game-context";
 import IngameClient from "@/features/game/components/InGameClient";
 import { SupportedLanguage } from "@/features/i18n/languages";
 import { loadTranslations } from "@/features/i18n/utils";
-import { useSocket } from "@/features/realtime/socket-context";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default async function GamePage({
@@ -27,62 +21,6 @@ export default async function GamePage({
     if (!game) {
         redirect(LANGUAGE_ROUTE(lang, PICK_GAME_MODE_ROUTE));
     }
-
-    
-
-    // const { account } = useAuth();
-    // const { initializeGameState, game, clearGameState } = useActiveGame();    
-    // const { initializeConnection, emitJoinGame, connectionStatus } = useSocket();
-    // const router = useRouter();
-
-
-    // useEffect(() => {
-    //     if (!gameId || !account) return;        
-
-    //     async function GetGame() {
-    //         if (!gameId || !account) return;
-            
-    //         try {
-    //             var resp = await GetActiveGameByIdRequest(gameId.toString());
-    //             if (!resp) {
-    //                 router.push(LANGUAGE_ROUTE(lang, PICK_GAME_MODE_ROUTE))
-    //                 return;
-    //             };
-                
-    //             initializeGameState(resp, account.id);               
-    //         } catch(err) {
-    //             console.log(err);
-    //             router.push(route(PICK_GAME_MODE_ROUTE));
-    //         }
-    //     }
-
-    //     GetGame();
-    // }, [gameId, account]);
-
-    // // Connect with realtime if online game
-    // useEffect(() => {
-    //     if (game && game.gameMode == "online") {
-    //         initializeConnection();
-    //     }
-    // }, [game]);
-
-    // // Join the game room when realtime is connected
-    // useEffect(() => {
-    //     if (!account || !game || connectionStatus != "connected") return;
-        
-    //     emitJoinGame({
-    //         gameId: game.id,
-    //         accountId: account.id,
-    //         username: account.username,
-    //         isHost: account.id == game?.hostAccountId
-    //     });         
-    // }, [connectionStatus, game, account]);
-
-    // useEffect(() => {
-    //     return () => {
-    //         clearGameState();
-    //     }
-    // }, []);
 
     return (
         <PageBase lang={lang} requiresAuh={true}>
--- letter-league/src/features/game/components/InGameClient.tsx
+++ letter-league/src/features/game/components/InGameClient.tsx
@@ -8,6 +8,8 @@ import GameResultOverview from "./GameResultOverview";
 import { useAuth } from "@/features/auth/AuthContext";
 import LoadingSpinner from "@/components/ui/animation/LoadingSpinner";
 import { SupportedLanguage } from "@/features/i18n/languages";
+import { useSocket } from "@/features/realtime/socket-context";
+import { useMessageBar } from "@/components/layout/MessageBarContext";
 
 interface Props {
     initialGameState: ActiveGameModel;
@@ -15,14 +17,46 @@ interface Props {
 }
 
 export default function IngameClient({ initialGameState, lang }: Props) {
-    const { initializeGameState, game, clearGameState, players } = useActiveGame();        
+    const { initializeGameState, game, clearGameState, players } = useActiveGame();    
+    const { initializeConnection, emitJoinGame, connectionStatus } = useSocket(); 
+    const { clearMessage, pushLoadingMsg } = useMessageBar();
     const { account } = useAuth();
 
+    // On client leave, clean game state
+    useEffect(() => {
+        return () => {
+            clearGameState();
+        }
+    }, []);    
+
+    // Initialize game
     useEffect(() => {
         if (!account) return;
-
         initializeGameState(initialGameState, account.id);
     }, [account]);
+
+    // Connect with realtime if online game
+    useEffect(() => {
+        if (connectionStatus == "connected") return;
+
+        if (game && game.gameMode == "online") {
+            pushLoadingMsg("Connecting with the realtime server");
+            initializeConnection();
+        }
+    }, [game]);
+
+    // Join the game room when realtime is connected
+    useEffect(() => {
+        if (!account || connectionStatus != "connected") return;
+
+        clearMessage();
+        emitJoinGame({
+            gameId: initialGameState.id,
+            accountId: account.id,
+            username: account.username,
+            isHost: account.id == game?.hostAccountId
+        });                      
+    }, [connectionStatus]);    
 
     if (!game) {
         return <LoadingSpinner size="lg" center={true} />;
