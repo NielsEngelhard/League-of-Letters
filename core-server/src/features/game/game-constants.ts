5048d03972e52bf0befdd834984879465515b05b
engelhardniels <engelhardniels@gmail.com>
2025-09-19 12:39:25 +0200

    small fix

--- core-server/src/features/game/game-constants.ts
+++ core-server/src/features/game/game-constants.ts
@@ -1,6 +1,6 @@
 // Game Settings
 export const MIN_TOTAL_ROUNDS: number = 1;
-export const MAX_TOTAL_ROUNDS: number = 6;
+export const MAX_TOTAL_ROUNDS: number = 16;
 
 export const MIN_GUESSES_PER_ROUND: number = 2;
 export const MAX_GUESSES_PER_ROUND: number = 8;
