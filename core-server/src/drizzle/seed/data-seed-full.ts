aa56c125ee489200ae0a0e2976ae61c925abb960
engelhardniels <engelhardniels@gmail.com>
2025-10-01 21:00:26 +0200

    update stats

--- core-server/src/drizzle/seed/data-seed-full.ts
+++ core-server/src/drizzle/seed/data-seed-full.ts
@@ -1,6 +1,6 @@
 import { seedWordsInDatabase } from "./data-seed-base";
 
-const dbConnectionString  = "postgresql://postgres:1797BBE575CC4BA9960982B37AE055EB@134.209.199.44:8082/league_of_letters";
+const dbConnectionString  = "postgresql://postgres:750B5AE609654A18A7B99E9503AC22FD@134.209.199.44:8082/league_of_letters";
 
 const isSmallSubset = false; // FULL WORDS
 seedWordsInDatabase(dbConnectionString, isSmallSubset);
